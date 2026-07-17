f4eeeb5db99df4ff59f7b2c85958aafc69db088b
Mehdi Talalha <m.talalha@gmail.com>
2026-07-18 00:49:59 +0100

    L1 : How to use SQLITE (STRUCTURE)

---
+++ STRUCTURE.py
@@ -0,0 +1,29 @@
+# TUTORIAL VIDEO : 
+# https://youtu.be/byHcYRpMgI4
+
+
+# import sqlite3 module
+import sqlite3
+
+# connect this python file to the database
+connect = sqlite3.connect("__DATABASE_NAME__.db")
+
+# CREATE A CURSOR
+cursor = connect.cursor()
+
+
+# to execute commands
+cursor.execute(""" 
+
+
+YOU_SQLITE_COMMAND_SHOULD_BE_HERE
+
+
+""")
+
+
+# commit your changes
+connect.commit()
+
+# stop the connection
+connect.close()
