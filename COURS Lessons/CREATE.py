457e9d5f279358138902031e96c04ac72d6a8e1b
Mehdi Talalha <m.talalha@gmail.com>
2026-07-18 01:27:04 +0100

    How to create a table

---
+++ COURS Lessons/CREATE.py
@@ -0,0 +1,35 @@
+# import sqlite3 module
+import sqlite3
+
+# connect this python file to the database
+connect = sqlite3.connect("__DATABASE_NAME__.db")
+
+# CREATE A CURSOR
+cursor = connect.cursor()
+
+
+# to execute commands on the datbase
+cursor.execute(""" CREATE TABLE table_name (
+	
+	user_name TEXT,
+	number_phone INTEGER,
+	email TEXT
+
+)""")
+
+# DATA TYPES ARE
+data_types = {
+ 
+    'INTEGER' : '1, 2, 3, 4..',
+    'TEXT' : 'STRINGS',
+    'REAL' : '0.1 | float',
+    'NULL' : 'None | doesn\'t exist',
+    'BLOB' : 'Binary object: stores data as it was input (e.g. images, files...)'
+    ''
+}
+
+# commit your changes
+connect.commit()
+
+# stop the connection
+connect.close()
